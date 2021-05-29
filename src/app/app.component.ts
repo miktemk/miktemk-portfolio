5d5ca70b2d5b8f7cd6fb18aaad6a566cfed040f3
Mikhail Temkine <miktemk@gmail.com>
2021-05-29 11:17:05 -0400

    upgrade to A12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { UIRouter, Transition } from '@uirouter/angular';
 import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
+const CurYear = new Date().getFullYear();
+
 @Component({
   selector: 'mik-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+
+  copyrightYear: number = CurYear;
 
   private transitionServiceOnSuccess$sub: Function;
 
